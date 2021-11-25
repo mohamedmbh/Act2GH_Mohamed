c330f56850ff7980a4c35c562fad6857dfb24453
mohamedmbh <mohamedbenhamzaa@gmail.com>
2021-11-25 13:48:36 +0100

    Nuevos cambios

--- main.java
+++ main.java
@@ -1,16 +1,36 @@
-public class test {
+public class main {
 
-	private static int[] array = new int[20];
+   private static int[] array = new int[30];
 
-	public static void main(String[] args) {
+   public static void main(String[] args) {
+      array = new int[] { 12, 234, 346, 457, 76, 68, 76, 45, 623, 57, 58, 67, 8, 54, 24, 3, 6, 476, 83, 24, 67, 63, 2, 6, 9, 2, 1, 6, 235, 56 };
+      numeroRepeticionesArray();
+   }
 
-	}
+   public static void insertarNumeros() {
+      for (int i = 0; i < array.length; i++) {
+         int n = (int) Math.random() * (10 + 1);
+         array[i] = n;
+      }
+   }
 
-	public static void insertarNumeros() {
-		for (int i = 0; i < 20; i++) {
-			int n = (int) Math.random() * (10 + 1);
-			array[i] = n;
-		}
-	}
+   public static void verContenidoArray() {
+      for (int i = 0; i < array.length; i++) {
+         System.out.println(array[i]);
+      }
+   }
+
+   public static void numeroRepeticionesArray() {
+      for (int i = 0; i < array.length; i++) {
+         int contador = 0;
+         
+         for (int x = 0; x < array.length; x++) {
+            if (array[i] == array[x])
+               contador++;
+         }
+
+         System.out.println("El número " + array[i] + " se repite " + contador + " veces.");
+      }
+   }
 
 }
